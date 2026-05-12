d3d7ea3a298723c9caa9bb516a46600d38f786f2
grebenetskaya <e12126394@student.tuwien.ac.at>
2026-05-12 18:36:15 +0200

    add image + accessability

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,26 +1,48 @@
-const { merge } = require('webpack-merge');
-const common = require('./webpack.common.js');
+const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const CopyPlugin = require('copy-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
-module.exports = merge(common, {
+module.exports = {
   mode: 'production',
+
+  entry: {
+    app: './.idea/js/main.js',
+  },
+
+  output: {
+    filename: 'js/[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+
   plugins: [
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: './.idea/index.html',
+      filename: 'index.html',
+      chunks: ['app'],
     }),
-    new CopyPlugin({
+
+    new CopyWebpackPlugin({
       patterns: [
+        { from: '.idea/css', to: 'css' },
         { from: 'img', to: 'img' },
-        { from: 'css', to: 'css' },
-        { from: 'js/vendor', to: 'js/vendor' },
-        { from: 'icon.svg', to: 'icon.svg' },
-        { from: 'favicon.ico', to: 'favicon.ico' },
-        { from: 'robots.txt', to: 'robots.txt' },
-        { from: 'icon.png', to: 'icon.png' },
-        { from: '404.html', to: '404.html' },
-        { from: 'site.webmanifest', to: 'site.webmanifest' },
+        { from: '.idea/about.html', to: 'about.html' },
+        { from: '.idea/accessibility.html', to: 'accessibility.html' },
+        { from: '.idea/contact.html', to: 'contact.html' },
+        { from: '.idea/license.html', to: 'license.html' },
+        { from: '.idea/privacy.html', to: 'privacy.html' },
+        { from: '.idea/shop.html', to: 'shop.html' },
+        { from: '.idea/_cookie-banner.html', to: '_cookie-banner.html' },
       ],
     }),
   ],
-});
+
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
